5290c9e5e2d018839df2a8605d9b977dc9b41bbd
antbessonov <anton@bessonov.org>
2022-08-02 19:36:08 +0700

    Revert "Удаление невостребованных полей WeatherInfo.kt и WeatherInfoDbModel.kt"
    This reverts commit ecf8f36d

--- app/src/main/java/com/example/weatherinnovosibirsk/data/database/WeatherInfoDbModel.kt
+++ app/src/main/java/com/example/weatherinnovosibirsk/data/database/WeatherInfoDbModel.kt
@@ -4,15 +4,20 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "weather")
-data class WeatherInfoDbModel(
+data class WeatherInfoDbModel (
     @PrimaryKey
     val dt: Int?,
     val name: String?,
     val speed: Int?,
     val deg: Int?,
+    val id: Int?,
+    val main: String?,
+    val description: String?,
     val icon: String?,
     val temp: Double?,
     val feelsLike: Double?,
+    val tempMin: Double?,
+    val tempMax: Double?,
     val pressure: Int?,
     val humidity: Int?,
 )
