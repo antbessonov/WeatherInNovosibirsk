f0f3d37b86dbe6f547325cc9aa3749c55d03381a
antbessonov <anton@bessonov.org>
2022-08-02 17:58:16 +0700

    Редактирование WeatherActivity.kt

--- app/src/main/java/com/example/weatherinnovosibirsk/presentation/WeatherActivity.kt
+++ app/src/main/java/com/example/weatherinnovosibirsk/presentation/WeatherActivity.kt
@@ -3,7 +3,10 @@ package com.example.weatherinnovosibirsk.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.example.weatherinnovosibirsk.R
 import com.example.weatherinnovosibirsk.databinding.ActivityWeatherBinding
+import com.example.weatherinnovosibirsk.domain.WeatherInfo
+import com.squareup.picasso.Picasso
 
 class WeatherActivity : AppCompatActivity() {
 
@@ -20,7 +23,33 @@ class WeatherActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel.weatherInfo.observe(this) {
-            binding.hellTv.text = it.dt.toString()
+            setValueView(it)
+        }
+    }
+
+    private fun setValueView(weatherInfo: WeatherInfo) {
+        with(binding) {
+            Picasso.get().load(weatherInfo.icon).into(weatherIconImage)
+            cityNameTv.text = weatherInfo.name
+            tempTv.text = String.format(
+                getString(R.string.temp_text),
+                weatherInfo.temp
+            )
+            tempFeelsLikeTv.text = String.format(
+                getString(R.string.temp_feels_like_text),
+                weatherInfo.feelsLike
+            )
+            humidityValueTv.text = weatherInfo.humidity
+            dtTv.text = String.format(
+                getString(R.string.dt_text),
+                weatherInfo.dt
+            )
+            pressureValueTv.text = weatherInfo.pressure
+            windSpeedAndDegValueTv.text = String.format(
+                getString(R.string.wind_speed_and_deg_value_text),
+                weatherInfo.speed,
+                weatherInfo.deg
+            )
         }
     }
 
