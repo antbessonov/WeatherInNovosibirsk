1c2fb5da7b29e00fff2f7f6b0db57545b7a484d4
antbessonov <anton@bessonov.org>
2022-08-02 18:13:53 +0700

    Создание WeatherRepositoryImpl.kt

---
+++ app/src/main/java/com/example/weatherinnovosibirsk/data/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.example.weatherinnovosibirsk.data.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.weatherinnovosibirsk.data.database.AppDatabase
+import com.example.weatherinnovosibirsk.data.mapper.WeatherMapper
+import com.example.weatherinnovosibirsk.domain.WeatherInfo
+import com.example.weatherinnovosibirsk.domain.WeatherRepository
+
+class WeatherRepositoryImpl(
+    private val application: Application,
+) : WeatherRepository {
+
+    private val weatherInfoDao = AppDatabase.getInstance(application).weatherInfoDao()
+    private val mapper = WeatherMapper()
+
+    override fun getWeatherInfo(): LiveData<WeatherInfo> {
+        return Transformations.map(weatherInfoDao.getWeatherInfo()) {
+            mapper.mapDbModelToEntity(it)
+        }
+    }
+
+    override fun loadData() {
+        TODO()
+    }
+}
